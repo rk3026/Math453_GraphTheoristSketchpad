68d9d6801a5a35c33c66bcef27b5351daee8958f
Dylan Gyori <gyoridylan@gmail.com>
2024-09-28 03:24:38 -0700

    Created GraphRenderer class which renders vertices

---
+++ GraphTheoristSketchpad/GraphTheoristSketchpad/Interface/GraphRenderer.cs
@@ -0,0 +1,45 @@
+﻿using ScottPlot;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphTheoristSketchpad.Interface
+{
+    internal class GraphRenderer : IPlottable
+    {
+        // data and customization options
+        double[] Xs { get; }
+        double[] Ys { get; }
+        public IScatterSource Data { get; }
+        public float Radius { get; set; } = 10;
+        IColormap Colormap { get; set; } = new ScottPlot.Colormaps.Turbo();
+
+        // items required by IPlottable
+        public bool IsVisible { get; set; } = true;
+        public IAxes Axes { get; set; } = new Axes();
+        public IEnumerable<LegendItem> LegendItems => LegendItem.None;
+        public AxisLimits GetAxisLimits() => new(Xs.Min(), Xs.Max(), Ys.Min(), Ys.Max());
+
+        public GraphRenderer(double[] xs, double[] ys)
+        { 
+            Xs = xs; Ys = ys;
+            this.Data = new ScottPlot.DataSources.ScatterSourceDoubleArray(xs, ys);
+        }
+
+        public void Render(RenderPack rp)
+        {
+            FillStyle FillStyle = new();
+            using SKPaint paint = new();
+            for (int i = 0; i < Xs.Length; i++)
+            {
+                Coordinates centerCoordinates = new(Xs[i], Ys[i]);
+                Pixel centerPixel = Axes.GetPixel(centerCoordinates);
+                FillStyle.Color = Colormap.GetColor(i / (Xs.Length - 1.0));
+                ScottPlot.Drawing.DrawCircle(rp.Canvas, centerPixel, Radius, FillStyle, paint);
+            }
+        }
+    }
+}
--- GraphTheoristSketchpad/GraphTheoristSketchpad/MainWindow.xaml.cs
+++ GraphTheoristSketchpad/GraphTheoristSketchpad/MainWindow.xaml.cs
@@ -3,11 +3,18 @@ using ScottPlot;
 using ScottPlot.WPF;
 using System.Windows;
 using System.Windows.Input;
+using GraphTheoristSketchpad.Interface;
 
 namespace GraphTheoristSketchpad
 {
     public partial class MainWindow : Window
     {
+        readonly double[] Xs = Generate.RandomAscending(10);
+        readonly double[] Ys = Generate.RandomSample(10);
+        readonly GraphRenderer graphRenderer;
+        //Scatter graphRenderer;
+        int? IndexBeingDragged = null;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -21,20 +28,18 @@ namespace GraphTheoristSketchpad
             axis.Right.IsVisible = false;
             axis.Top.IsVisible = false;
 
-            Scatter = GraphView.Plot.Add.Scatter(Xs, Ys);
-            Scatter.LineWidth = 2;
-            Scatter.MarkerSize = 10;
-            Scatter.Smooth = true;
+            graphRenderer = new GraphRenderer(Xs, Ys);
+            //graphRenderer.LineWidth = 2;
+            //graphRenderer.MarkerSize = 10;
+            //graphRenderer.Smooth = true;
+
+            GraphView.Plot.Add.Plottable(graphRenderer);
+            //graphRenderer = GraphView.Plot.Add.ScatterPoints(Xs, Ys);
 
             GraphView.MouseMove += FormsPlot1_MouseMove;
             GraphView.MouseDown += FormsPlot1_MouseDown;
             GraphView.MouseUp += FormsPlot1_MouseUp;
         }
-
-        readonly double[] Xs = Generate.RandomAscending(10);
-        readonly double[] Ys = Generate.RandomSample(10);
-        readonly ScottPlot.Plottables.Scatter Scatter;
-        int? IndexBeingDragged = null;
 
         // Helper method to get DPI scaling factor
         private double GetDpiScale()
@@ -50,7 +55,7 @@ namespace GraphTheoristSketchpad
             Pixel mousePixel = new Pixel(e.GetPosition(GraphView).X * dpiScale, e.GetPosition(GraphView).Y * dpiScale);
 
             Coordinates mouseLocation = GraphView.Plot.GetCoordinates(mousePixel);
-            DataPoint nearest = Scatter.Data.GetNearest(mouseLocation, GraphView.Plot.LastRender);
+            DataPoint nearest = graphRenderer.Data.GetNearest(mouseLocation, GraphView.Plot.LastRender);
             IndexBeingDragged = nearest.IsReal ? nearest.Index : null;
 
             if (IndexBeingDragged.HasValue)
@@ -71,7 +76,7 @@ namespace GraphTheoristSketchpad
             Pixel mousePixel = new Pixel(e.GetPosition(GraphView).X * dpiScale, e.GetPosition(GraphView).Y * dpiScale);
 
             Coordinates mouseLocation = GraphView.Plot.GetCoordinates(mousePixel);
-            DataPoint nearest = Scatter.Data.GetNearest(mouseLocation, GraphView.Plot.LastRender);
+            DataPoint nearest = graphRenderer.Data.GetNearest(mouseLocation, GraphView.Plot.LastRender);
             GraphView.Cursor = nearest.IsReal ? Cursors.Hand : Cursors.Arrow;
 
             if (IndexBeingDragged.HasValue)
